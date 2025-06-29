93d21c2521a0120d9a97adc03e7889e2cea9d87d
BKVDPrasad <54040086+BKVDPrasad@users.noreply.github.com>
2025-06-30 00:55:01 +0530

    Update urls.py

--- mine/api/urls.py
+++ mine/api/urls.py
@@ -7,9 +7,25 @@ from .views import (
     AttendeeListAPIView
 )
 
+from drf_spectacular.views import (
+  SpectacularAPIView,
+  SpectacularSwaggerView,
+  SpectacularRedocView,
+)
+
 urlpatterns = [
     path('events/', EventListCreateAPIView.as_view(), name='event-list-create'),
     path('events/<int:pk>/', EventRetrieveUpdateDestroyAPIView.as_view(), name='event-detail'),
     path('events/<int:event_id>/register/', RegisterAttendeeAPIView.as_view(), name='event-register-attendee'),
     path('events/<int:event_id>/attendees/', AttendeeListAPIView.as_view(), name='event-attendee-list'),
+
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Swagger UI
+    path('schema/swagger-ui/',
+         SpectacularSwaggerView.as_view(url_name='schema'),
+         name='swagger-ui'),
+    # ReDoc UI
+    path('schema/redoc/',
+         SpectacularRedocView.as_view(url_name='schema'),
+         name='redoc'),
 ]
